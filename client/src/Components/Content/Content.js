536b9a61d26887f63dd65eac8765301d1ed235e6
jjmetty <jaredmetty123@gmail.com>
2023-03-26 23:50:52 -0500

    initialize home content

--- client/src/Components/Content/Content.js
+++ client/src/Components/Content/Content.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './ContentStyle.css'
+import Home from '../Home/Home'
 
 export default function Content(){
     return(
         <div className="content-container">
-           <h1>contnet</h1>
+           <Home />
         </div>
     )
 }
---
+++ client/src/Components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import '../Home/HomeStyle.css'
+
+
+export default function Home() {
+    return(
+       <div className="home-container">
+        <p>Home</p>
+       </div>
+    )
+}
