48db8d3d01d04716d401a59cff53e0174aa246d6
jjmetty <jaredmetty123@gmail.com>
2023-04-05 23:23:37 -0500

    add table styling

--- client/src/Components/ExpenseTable/ExpenseTable.js
+++ client/src/Components/ExpenseTable/ExpenseTable.js
@@ -2,6 +2,7 @@ import React from "react";
 import './ExpenseTableStyle.css'
 
 export default function ExpenseTable(){
+    //update even row background color
     return(
             <table className="expense-table content-box-shadow">
                 <thead>
@@ -14,7 +15,48 @@ export default function ExpenseTable(){
                     </tr>
                 </thead>
                 <tbody>
-                    
+                    <tr>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                    </tr>
+                    <tr>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                    </tr>
+                    <tr>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                    </tr>
+                    <tr>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                    </tr>
+                    <tr>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                    </tr>
+                    <tr>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                        <td>test</td>
+                    </tr>
                 </tbody>
             </table>
     )
