a1aa4dc7baf4e4799ec98eede6c9e0daa1eb3dfc
jjmetty <jaredmetty123@gmail.com>
2023-04-09 21:15:07 -0500

    [Client] Get data from api and display in table

--- client/src/Components/ExpenseTable/ExpenseTable.js
+++ client/src/Components/ExpenseTable/ExpenseTable.js
@@ -1,9 +1,24 @@
 import React from "react";
 import './ExpenseTableStyle.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function ExpenseTable(){
-    //update even row background color
-    //add new expense button and create a search bar on right side
+export default function ExpenseTable({Expenses}){
+
+    const formatDate = (date) =>{
+        let dateFormat = new Date(date)
+        return (dateFormat.getMonth() + 1) + '/' + dateFormat.getDate() + '/' +  dateFormat.getFullYear()
+    }
+
+    const mapExpenses = Expenses?.map(expense =>
+        <tr key={expense._id}>
+            <td>{expense.expense}</td>
+            <td>{expense.amount}</td>
+            <td>{expense.type}</td>
+            <td>{formatDate(expense.date)}</td>
+            <td><FontAwesomeIcon icon="fa-solid fa-trash" style={{cursor: 'pointer'}}/></td>
+        </tr>
+    )
+
     return(
             <table className="expense-table content-box-shadow">
                 <thead>
@@ -16,48 +31,7 @@ export default function ExpenseTable(){
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>test</td>
-                        <td>test</td>
-                        <td>test</td>
-                        <td>test</td>
-                        <td>test</td>
-                    </tr>
-                    <tr>
-                        <td>test</td>
-                        <td>test</td>
-                        <td>test</td>
-                        <td>test</td>
-                        <td>test</td>
-                    </tr>
-                    <tr>
-                        <td>test</td>
-                        <td>test</td>
-                        <td>test</td>
-                        <td>test</td>
-                        <td>test</td>
-                    </tr>
-                    <tr>
-                        <td>test</td>
-                        <td>test</td>
-                        <td>test</td>
-                        <td>test</td>
-                        <td>test</td>
-                    </tr>
-                    <tr>
-                        <td>test</td>
-                        <td>test</td>
-                        <td>test</td>
-                        <td>test</td>
-                        <td>test</td>
-                    </tr>
-                    <tr>
-                        <td>test</td>
-                        <td>test</td>
-                        <td>test</td>
-                        <td>test</td>
-                        <td>test</td>
-                    </tr>
+                   {mapExpenses}
                 </tbody>
             </table>
     )
--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import '../Home/HomeStyle.css'
 import Budget from "../Budget/Budget"
 import HomeGraph from "../HomeGraph/HomeGraph";
 import ExpenseTable from "../ExpenseTable/ExpenseTable";
 import TableFunctions from "../TableFunctions/TableFunctions"
+import axios from 'axios'
 
 
 export default function Home() {
+
+    //create url for api calls
+    const client = axios.create({
+        baseURL: "http://localhost:4000/api/"
+    })
+
+    //state for expenses 
+    const [apiExpenses, setapiExpenses] = useState([]);
+    const [isCreatingExpense, setisCreatingExpense] = useState(false);
+
+    //get expenses from api
+    useEffect(() => {
+        const getData = async () =>{
+            try{
+                let response = await client.get('expenses');
+                setapiExpenses(response.data);
+            } catch(error){
+                console.log(error);
+            }
+
+        }
+        getData();
+        // eslint-disable-next-line
+    }, []);
 
     return(
        <div className="home-container">
@@ -14,8 +39,8 @@ export default function Home() {
             <Budget />
             <HomeGraph />
         </div>
-        <TableFunctions />
-        <ExpenseTable />
+        <TableFunctions setisCreatingExpense = {setisCreatingExpense} />
+        <ExpenseTable Expenses = {apiExpenses}/>
        </div>
     )
 }
