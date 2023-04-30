78a90b5220133b769d98666d493e9843e5481a88
jjmetty <jaredmetty123@gmail.com>
2023-04-30 17:15:00 -0500

    [Client] update sidebar position and hover

--- client/src/Components/Sidebar/Sidebar.js
+++ client/src/Components/Sidebar/Sidebar.js
@@ -8,11 +8,12 @@ export default function Sidebar (){
     //on hover for mybudgetter make gradient color 
     return(
         <div className="sidebar-container">
+            <br />
             <ul className="sidebar-list">
-                <li className="sidebar-links"><FontAwesomeIcon icon="fa-solid fa-house" className="sidebar-icons"/> &nbsp; Home</li>
-                <li className="sidebar-links"><FontAwesomeIcon icon="fa-solid fa-chart-simple" className="sidebar-icons"/> &nbsp; Graphs</li>
-                <li className="sidebar-links"><FontAwesomeIcon icon="fa-solid fa-calendar" className="sidebar-icons"/> &nbsp; Calendar</li>
-                <li className="sidebar-links"><FontAwesomeIcon icon="fa-solid fa-file" className="sidebar-icons"/> &nbsp; Reports</li>
+                <li className="sidebar-links"><FontAwesomeIcon icon="fa-solid fa-house" className="sidebar-icons"/><div className="sidebar-title">Home</div></li>
+                <li className="sidebar-links"><FontAwesomeIcon icon="fa-solid fa-chart-simple" className="sidebar-icons"/><div className="sidebar-title">Graphs</div></li>
+                <li className="sidebar-links"><FontAwesomeIcon icon="fa-solid fa-calendar" className="sidebar-icons"/><div className="sidebar-title">Calendar</div></li>
+                <li className="sidebar-links"><FontAwesomeIcon icon="fa-solid fa-file" className="sidebar-icons"/><div className="sidebar-title">Reports</div></li>
             </ul>
             
         </div>
