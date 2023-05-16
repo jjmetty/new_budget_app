50110fb9b77c977304a903d30a27426afd1e3055
jjmetty <jaredmetty123@gmail.com>
2023-05-15 23:22:12 -0500

    [Client] add current month and restyle new expense and table filter

--- client/src/Components/FilterExpenseTable/FilterExpenseTable.js
+++ client/src/Components/FilterExpenseTable/FilterExpenseTable.js
@@ -3,6 +3,8 @@ import './FilterExpenseTableStyle.css'
 
 export default function FilterExpenseTable(){
     return(
+        <div className="filter-table-container">
         <input type="text" className="filter-expense-input" placeholder="Filter Table..."/>
+        </div>
     )
 }
--- client/src/Components/TableFunctions/TableFunctions.js
+++ client/src/Components/TableFunctions/TableFunctions.js
@@ -4,9 +4,14 @@ import NewExpenseButton from "../NewExpenseButton/NewExpenseButton";
 import FilterExpenseTable from "../FilterExpenseTable/FilterExpenseTable";
 
 export default function TableFunctions({setisCreatingExpense}){
+
+    const currentDate = new Date();
+    const currentMonth = currentDate.toLocaleString('default', {month: 'long'});
+
     return(
         <div className="table-functions-container">
             <NewExpenseButton setisCreatingExpense = {setisCreatingExpense}/>
+            <h2>{currentMonth}</h2>
             <FilterExpenseTable />
         </div>
     )
