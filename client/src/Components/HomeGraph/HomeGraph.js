e876ee7418e5b4ea4bdd6a69cdd43a2d66d3d960
jjmetty <jaredmetty123@gmail.com>
2023-05-29 23:50:39 -0500

    [Client] update labels to show on left side, placeholder graph colors

--- client/src/Components/HomeGraph/HomeGraph.js
+++ client/src/Components/HomeGraph/HomeGraph.js
@@ -29,22 +29,58 @@ export default function HomeGraph(){
         }
     }
 
+    //labels sorted alphabetically
+    //.map(x => x + `${x.amount}`) add to show percentage of graph
     let graphLabels = Object.keys(sumAmount);
 
+    //data sorted alphabetically
     let graphData = Object.values(sumAmount);
 
     const data ={
         labels: graphLabels,
         datasets: [{
-            data: graphData
+            data: graphData,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(73, 180, 20, 0.2)',
+                'rgba(100, 50, 80, 0.2)'
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(73, 180, 20, 1)',
+                'rgba(100, 50, 80, 1)'
+              ],
+              borderWidth: 1,
         }]
+    }
+
+    const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                position: "right"
+            }
+        }
     }
 
     return (
         <div className="home-graph-container content-box-shadow">
-            <Doughnut
-            data = {data}
-            ></Doughnut>
+            <div className="graph-wrapper">
+                <Doughnut
+                data = {data} options={options} 
+                ></Doughnut>
+            </div>
         </div>
     )
 }
