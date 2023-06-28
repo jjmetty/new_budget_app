e45c79ad3c258807bc177adfb954fae943394993
jjmetty <jaredmetty123@gmail.com>
2023-06-27 23:26:46 -0500

    [Client] add login base layout

--- client/src/Components/LoginComponents/LoginPage.js
+++ client/src/Components/LoginComponents/LoginPage.js
@@ -3,7 +3,11 @@ import '../LoginComponents/LoginPageStyle.css'
 
 export default function LoginPage() {
     return(
-        <h1 className="login creation">Creation in Progress</h1>
+        <div className="login-wrapper">
+            <div className="login-container">
+            <input type="text" />
+            </div>
+        </div>
     )
     
 }
