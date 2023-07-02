03e9e8dffeeb31368333a4206ece5d99bb020fc7
jjmetty <jaredmetty123@gmail.com>
2023-07-02 18:30:54 -0500

    [Client] create login form

--- client/src/Components/LoginComponents/LoginPage.js
+++ client/src/Components/LoginComponents/LoginPage.js
@@ -5,7 +5,10 @@ export default function LoginPage() {
     return(
         <div className="login-wrapper">
             <div className="login-container">
-            <input type="text" />
+            <h2 style={{color: "white"}}>Login</h2>
+            <input type="text" placeholder="Email"/>
+            <input type="text" placeholder="Password"/>
+            <button className="login-button">Login</button>
             </div>
         </div>
     )
